f344c3d2fa950dd3d3a6db03bc0b341a1bb80e00
sergivonavi <2625014+sergivonavi@users.noreply.github.com>
2025-09-22 19:50:17 +0300

    Initial commit

---
+++ src/test/java/ru/azbooka/at/tests/BaseTest.java
@@ -0,0 +1,54 @@
+package ru.azbooka.at.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.NoSuchSessionException;
+import ru.azbooka.at.utils.report.AllureAttachments;
+
+import static com.codeborne.selenide.WebDriverRunner.hasWebDriverStarted;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+
+        Configuration.baseUrl = "https://azbooka.ru";
+        RestAssured.baseURI = "https://api.azbooka.ru";
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void afterEach() {
+        if (hasWebDriverStarted()) {
+            try {
+                attachArtifacts();
+            } finally {
+                Selenide.closeWebDriver();
+            }
+        }
+    }
+
+    private void attachArtifacts() {
+        try {
+            AllureAttachments.attachScreenshot("Final state");
+            AllureAttachments.attachVideo();
+            AllureAttachments.attachPageSource();
+            AllureAttachments.attachBrowserConsoleLogs();
+        } catch (NoSuchSessionException e) {
+            System.out.println("WebDriver session already closed, skipping artifacts");
+        }
+    }
+}
---
+++ src/test/java/ru/azbooka/at/utils/report/AllureAttachments.java
@@ -0,0 +1,62 @@
+package ru.azbooka.at.utils.report;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+public class AllureAttachments {
+
+    @Attachment(value = "{name}", type = "image/png")
+    public static byte[] attachScreenshot(String name) {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "Page source", type = "text/plain")
+    public static byte[] attachPageSource() {
+        return WebDriverRunner.getWebDriver()
+                .getPageSource()
+                .getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Attachment(value = "{name}", type = "text/plain")
+    public static String attachText(String name, String message) {
+        return message;
+    }
+
+    public static void attachBrowserConsoleLogs() {
+        attachText(
+                "Browser console logs",
+                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+        );
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String attachVideo() {
+        return "<html>" +
+                "<body>" +
+                "<video width='100%' height='100%' controls autoplay>" +
+                "<source src='" + getVideoUrl() + "' type='video/mp4'>" +
+                "</video>" +
+                "</body>" +
+                "</html>";
+    }
+
+    public static URL getVideoUrl() {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + Selenide.sessionId() + ".mp4";
+        try {
+            return URI.create(videoUrl).toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
