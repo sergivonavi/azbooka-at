cb1dd3b5377fbef60a71b0b6660dc81f76b86873
sergivonavi <2625014+sergivonavi@users.noreply.github.com>
2025-09-23 01:04:46 +0300

    Add user API tests
    Closes #6

---
+++ src/test/java/ru/azbooka/at/data/UserTestData.java
@@ -0,0 +1,11 @@
+package ru.azbooka.at.data;
+
+public class UserTestData {
+    public static final String DEFAULT_AVATAR = "avatar/avatar_default.jpg";
+    public static final String DEFAULT_FIRST_NAME = "Иван";
+    public static final String DEFAULT_LAST_NAME = "Иванов";
+
+    public static final String AVATAR = "avatar/avatar1.jpg";
+    public static final String FIRST_NAME = "Семён";
+    public static final String LAST_NAME = "Семёнов";
+}
