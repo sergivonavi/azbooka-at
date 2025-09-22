469a5ee74b29df4ceb2519f8da0d3c9a13e54c5e
sergivonavi <2625014+sergivonavi@users.noreply.github.com>
2025-09-22 21:04:55 +0300

    Add login UI tests
    Closes #4

---
+++ src/test/java/ru/azbooka/at/ui/pages/BasePage.java
@@ -0,0 +1,35 @@
+package ru.azbooka.at.ui.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class BasePage {
+    private final SelenideElement loginButton = $x("//span[contains(@class, 'fa-user') and not(ancestor::nav)]/ancestor::button");
+    private final SelenideElement signOutButton = $x("//header//span[contains(@class,'fa-sign-out') and not(ancestor::nav)]");
+    private final SelenideElement profileLink = $x("//a[@href='/user/']");
+
+    @Step("Нажимаем кнопку \"Вход\"")
+    public void clickLoginButton() {
+        loginButton
+                .shouldBe(visible)
+                .shouldHave(text("Вход"))
+                .click();
+    }
+
+    @Step("Проверяем, что ссылка на страницу профиля отображается")
+    public BasePage shouldHaveProfileLink() {
+        profileLink.shouldBe(visible);
+        return this;
+    }
+
+    @Step("Проверяем, что кнопка выхода отображается")
+    public BasePage shouldHaveSignOutButton() {
+        signOutButton.should(exist);
+        return this;
+    }
+}
